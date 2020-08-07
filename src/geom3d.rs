4f6a3a8d71df3a16808d4430d11950edd5659ce1
Sébastien Besnier <sebastien.fabrice.besnier@gmail.com>
2020-08-07 07:16:34 +0200

    add reflexion on multiple spheres

--- src/geom3d.rs
+++ src/geom3d.rs
@@ -1,6 +1,6 @@
 use std::ops::{Add, Div, Mul, Sub};
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Vect {
     pub x: f64,
     pub y: f64,
@@ -8,10 +8,18 @@ pub struct Vect {
 }
 
 impl Vect {
+    pub const NULL: Vect = Vect::new(0.0, 0.0, 0.0);
     pub fn dot(self, other: Vect) -> f64 {
         self.x * other.x + self.y * other.y + self.z * other.z
     }
 
+    pub fn cross(self, other: Vect) -> Vect {
+        Vect {
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
+        }
+    }
     pub const fn new(x: f64, y: f64, z: f64) -> Vect {
         Vect { x: x, y: y, z: z }
     }
@@ -24,8 +32,8 @@ impl Vect {
         f64::sqrt(self.norm2())
     }
 
-    pub fn unit(self) -> Vect {
-        self / self.norm()
+    pub fn unit(self) -> Unit {
+        Unit(self / self.norm())
     }
 
     pub fn as_vec_u8(self) -> [u8; 3] {
@@ -33,7 +41,42 @@ impl Vect {
     }
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Unit(Vect);
+
+impl Unit {
+    pub fn to_vect(&self) -> Vect {
+        self.0
+    }
+
+    pub fn cross(self, other: Unit) -> Unit {
+        self.to_vect().cross(other.to_vect()).unit()
+    }
+
+    pub fn dot(self, other: Unit) -> f64 {
+        self.to_vect().dot(other.to_vect())
+    }
+}
+
+/// Returns the vector v reflected in respect to the
+/// first coordinate of the (e1, e2, e3) basis.
+///
+/// (e1, e2, e3) is supposed to be an orthonormal basis
+pub fn x_reflexion(v: Unit, (e1, e2, e3): (Unit, Unit, Unit)) -> Unit {
+    /* (x_, y_, z_) are the coordinates of v in the
+       (e1, e2, e3) basis
+    */
+    let x_ = v.to_vect().dot(e1.to_vect());
+    let y_ = v.to_vect().dot(e2.to_vect());
+    let z_ = v.to_vect().dot(e3.to_vect());
+
+    /* Converting back to the canonical basis, reverting
+       the forst coordinate (hence the - sign).
+    */
+    Unit((-x_ * e1.to_vect()) + y_ * e2.to_vect() + z_ * e3.to_vect())
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Point {
     pub x: f64,
     pub y: f64,
@@ -125,5 +168,30 @@ impl Div<f64> for Vect {
             y: self.y / other,
             z: self.z / other,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::geom3d::*;
+
+    #[test]
+    fn unit_test() {
+        let e1 = Vect::new(1.0, 0.0, 0.0);
+        let e2 = Vect::new(0.0, 1.0, 0.0);
+        let e3 = Vect::new(0.0, 0.0, 1.0);
+        assert_eq!(e1, e1.unit().to_vect());
+        assert_eq!(e2, e2.unit().to_vect());
+        assert_eq!(e3, e3.unit().to_vect());
+    }
+
+    #[test]
+    fn x_reflexion_test() {
+        let ray = Vect::new(1.0, 0.0, -1.0).unit();
+        let e1 = Vect::new(1.0, 0.0, 0.0).unit();
+        let e2 = Vect::new(0.0, 1.0, 0.0).unit();
+        let e3 = Vect::new(0.0, 0.0, 1.0).unit();
+        let x_ref = x_reflexion(ray, (e1, e2, e3));
+        assert_eq!(x_ref, Vect::new(-1.0, 0.0, -1.0).unit());
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,24 @@
 mod geom3d;
-use geom3d::{Point, Vect};
+use geom3d::{Point, Unit, Vect};
 
 extern crate image;
 
 type Color = image::Rgb<u8>;
 
 mod sphere3d {
-    use crate::geom3d::{Point, Vect};
+    use crate::geom3d::{Point, Unit, Vect};
 
     pub struct Sphere3D {
+        pub id: u32,
         center: Point,
         color: Vect,
         squared_radius: f64,
     }
 
     impl Sphere3D {
-        pub fn new(center: Point, radius: f64, color: crate::Color) -> Sphere3D {
+        pub fn new(id: u32, center: Point, radius: f64, color: crate::Color) -> Sphere3D {
             Sphere3D {
+                id: id,
                 center: center,
                 squared_radius: radius * radius,
                 color: Vect::new(color[0] as f64, color[1] as f64, color[2] as f64),
@@ -35,8 +37,8 @@ mod sphere3d {
             self.color
         }
 
-        pub fn trace(&self, start: Point, direction: Vect) -> f64 {
-            let minus_b_over_2 = direction.dot(start.to(self.center()));
+        pub fn trace(&self, start: Point, direction: Unit) -> f64 {
+            let minus_b_over_2 = direction.to_vect().dot(start.to(self.center()));
             let c = self.center.to(start).norm2() - self.squared_radius();
             let reduced_delta = minus_b_over_2 * minus_b_over_2 - c;
             if reduced_delta < 0.0 {
@@ -57,7 +59,20 @@ mod sphere3d {
                 f64::max(minus_b_over_2, 0.0)
             }
         }
+
+        pub fn normal_vector(&self, point: Point) -> Unit {
+            self.center().to(point).unit()
+        }
     }
+}
+
+fn bounce(ray: Unit, normal: Unit) -> Unit {
+    /* Building an orthonormal (e1, e2, e3) basis */
+    let e1 = normal;
+    let e2 = e1.cross(ray);
+    let e3 = e1.cross(e2);
+
+    geom3d::x_reflexion(ray, (e1, e2, e3))
 }
 
 use sphere3d::Sphere3D;
@@ -70,25 +85,67 @@ fn screen_to_space(x: u32, y: u32) -> Point {
     )
 }
 
-fn color(x: u32, y: u32, spheres: &[Sphere3D]) -> Color {
-    let direction = Point::ORIGIN.to(screen_to_space(x, y)).unit();
-    let mut distances_spheres = spheres
+use std::ptr;
+
+fn first_collision<'a>(
+    start: Point,
+    direction: Unit,
+    maybe_last_sphere: Option<&'a Sphere3D>,
+    objects: &'a [Sphere3D],
+) -> Option<(f64, &'a Sphere3D)> {
+    let mut distances_spheres = objects
         .into_iter()
-        .map(|sphere| (sphere.trace(Point::ORIGIN, direction), sphere))
-        .filter(|(dist, _)| dist > &0.0)
-        .collect::<Vec<(f64, &Sphere3D)>>();
+        .map(|sphere| (sphere.trace(start, direction), sphere))
+        .filter(|(dist, sphere)| match maybe_last_sphere {
+            None => dist > &0.0,
+            Some(last_sphere) => dist > &0.0 && sphere.id != last_sphere.id,
+        })
+        .collect::<Vec<(f64, &'a Sphere3D)>>();
     distances_spheres.sort_by(|(dist1, _), (dist2, _)| dist1.partial_cmp(dist2).unwrap());
 
-    match distances_spheres.get(0) {
-        None => BACKGROUND_COLOR,
-        Some((distance, sphere)) => {
-            let m = Point::ORIGIN + *distance * direction;
-            let ml = m.to(LIGHT_SOURCE);
-            let l = ml.unit();
-            let n = (sphere.center().to(m)).unit();
-            let luminosity = f64::max(l.dot(n), 0.0);
-            image::Rgb((luminosity * sphere.color()).as_vec_u8())
+    distances_spheres.get(0).map(|x| *x)
+}
+
+fn color(observer: Point, screen_point: Point, spheres: &[Sphere3D]) -> Color {
+    let mut current_point = observer;
+    let mut current_direction = observer.to(screen_point).unit();
+    let mut current_color = None;
+    let mut last_sphere = None;
+    let mut reflexion_count = 0;
+
+    //println!("{:?}", current_point);
+    while let Some((distance, sphere)) =
+        first_collision(current_point, current_direction, last_sphere, spheres)
+    {
+        // print!(
+        //     "*******\npoint:{:?}\ndirection:{:?}\ndistance:{}\n",
+        //     current_point, current_direction, distance
+        // );
+        current_point = current_point + distance * current_direction.to_vect();
+        //println!("{}, {:?}, {:?}", distance, current_point, current_direction);
+        current_direction = bounce(current_direction, sphere.normal_vector(current_point));
+        last_sphere = Some(sphere);
+        reflexion_count += 1;
+        let ml = current_point.to(LIGHT_SOURCE);
+        let l = ml.unit();
+        let n = (sphere.center().to(current_point)).unit();
+        let luminosity = f64::max(l.dot(n), 0.0);
+        let sphere_color = luminosity * sphere.color();
+        current_color = Some(add_color(sphere_color, current_color));
+
+        if reflexion_count > 3 {
+            //break;
         }
+    }
+    current_color
+        .map(|v| image::Rgb(v.as_vec_u8()))
+        .unwrap_or(BACKGROUND_COLOR)
+}
+
+fn add_color(color: Vect, current_color: Option<Vect>) -> Vect {
+    match current_color {
+        None => color,
+        Some(current) => 0.5 * current + 0.5 * color,
     }
 }
 
@@ -104,12 +161,42 @@ const BACKGROUND_COLOR: Color = image::Rgb([50, 50, 50]);
 pub fn create_image() -> image::ImageBuffer<Color, std::vec::Vec<u8>> {
     let mut imgbuf = image::ImageBuffer::new(WIDTH, HEIGHT);
     let spheres = [
-        Sphere3D::new(Point::new(0.0, 5.0, 10.0), 1.0, image::Rgb([216, 84, 52])),
-        Sphere3D::new(Point::new(0.0, 0.0, 10.0), 1.0, image::Rgb([52, 84, 216])),
+        Sphere3D::new(
+            1,
+            Point::new(0.0, 5.0, 10.0),
+            1.0,
+            image::Rgb([216, 84, 52]),
+        ),
+        Sphere3D::new(
+            2,
+            Point::new(0.0, 0.0, 10.0),
+            1.0,
+            image::Rgb([52, 84, 216]),
+        ),
+        Sphere3D::new(
+            3,
+            Point::new(5.0, -5.0, 15.0),
+            5.0,
+            image::Rgb([84, 216, 52]),
+        ),
     ];
     // Iterate over the coordinates and pixels of the image
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        *pixel = color(x, y, &spheres);
+        *pixel = color(Point::ORIGIN, screen_to_space(x, y), &spheres);
     }
+    //color(Point::ORIGIN, screen_to_space(961, 537), &spheres);
     imgbuf
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::geom3d::*;
+
+    #[test]
+    fn bounce_test() {
+        let ray = Vect::new(1.0, 0.0, -1.0).unit();
+        let normal = Vect::new(0.0, 0.0, 1.0).unit();
+        let z_ref = crate::bounce(ray, normal);
+        assert_eq!(z_ref, Vect::new(1.0, 0.0, 1.0).unit());
+    }
 }
